b20371d2710d5e5684d564ba5c28b16664d4f9a8
Yanters <yanter@wp.pl>
2023-06-29 15:05:03 +0200

    Base for themes

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,27 @@
-import Image from 'next/image';
+'use client'
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), [])
+
+  if (!mounted) return null
+
   return (
     <div className='container flex flex-col items-center justify-center min-h-screen min-w-full py-2'>
-      <h1 className='text-6xl font-bold text-center text-cyan-100'>
+      <h1 className='text-6xl font-bold text-center dark:text-cyan-100 text-cyan-500'>
         Studenciak
       </h1>
+      <div>
+        The current theme is: {theme}
+        <br />
+        <button onClick={() => setTheme('light')}>Light Mode</button>{' '}
+        <button onClick={() => setTheme('dark')}>Dark Mode</button>
+      </div>
     </div>
-  );
+  )
 }
