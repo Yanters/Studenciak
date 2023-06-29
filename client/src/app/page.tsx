5b1f24d897bce20a62ceca649022cd99979fc7e1
Yanters <yanter@wp.pl>
2023-06-29 14:34:30 +0200

    Starting files

---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className='container flex flex-col items-center justify-center min-h-screen min-w-full py-2'>
+      <h1 className='text-6xl font-bold text-center text-cyan-100'>
+        Studenciak
+      </h1>
+    </div>
+  );
+}
